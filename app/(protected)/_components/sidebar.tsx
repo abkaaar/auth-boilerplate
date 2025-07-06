dc86aac737412ab2acf5f3b956ccf7123c9c26ce
abkaaar <abdullahabubakar461@gmail.com>
2025-07-06 22:47:42 +0100

    Refactor sidebar layout and improve styling for better user experience

--- app/(protected)/_components/sidebar.tsx
+++ app/(protected)/_components/sidebar.tsx
@@ -60,30 +60,39 @@ function SideBarF({isOpen}: SideBarProps) {
 
       {/* Sidebar */}
     <aside
-        className={`fixed top-16 left-0 z-40 w-64 h-[calc(100vh-4rem)] transition-transform ${
+        className={`fixed top-0 left-0 z-40 w-64 h-[calc(100vh-4rem)] transition-transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } sm:translate-x-0 `}
         aria-label="Sidebar"
        >
-        <div className="h-full flex flex-col px-3 py-10 overflow-y-auto bg-white dark:bg-gray-800">
+        <div className="h-full flex flex-col p-3 overflow-y-auto bg-white dark:bg-gray-800">
+            <div className="flex p-2 mb-6">
+              <Package className="w-8 h-8 text-blue-600" />
+              <span className="text-xl font-semibold text-blue-600">
+                Edupad
+              </span>
+            </div>
+        <div className="flex flex-col h-full">
           <ul className="space-y-2 font-medium flex-1">
             {items.map((item) => (
-             <li key={item.title}>
-               <Link
-                href={item.url}
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-               >
-                <item.icon />
-                <span className="ms-3">{item.title}</span>
-               </Link>
-             </li>
+              <li key={item.title}>
+                <Link
+                  href={item.url}
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <item.icon />
+                  <span className="ms-3">{item.title}</span>
+                </Link>
+              </li>
             ))}
           </ul>
-         
-       <LogoutButton>
-        <ExitIcon/>
-        <span>Logout</span>
-       </LogoutButton>
+          <div className="mt-auto">
+            <LogoutButton>
+              <ExitIcon />
+              <span>Logout</span>
+            </LogoutButton>
+          </div>
+        </div>
         </div>
        </aside>
 
